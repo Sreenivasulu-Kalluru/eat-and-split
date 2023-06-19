a6b03ea2afb089e535043a30b611cbe1b626bc3d
Vasu Kalluru <84690382+KALLURUSREENIVASULU@users.noreply.github.com>
2023-06-19 15:52:53 +0530

    updates git commit -m updates git commit -m "updates

--- src/App.js
+++ src/App.js
@@ -81,7 +81,7 @@ export default function App() {
         <FormSplitBill
           selectedFriend={selectedFriend}
           onSplitBill={handleSplitBill}
-          key={selectedFriend.id}
+          key={selectedFriend.id} // key prop
         />
       )}
     </div>
